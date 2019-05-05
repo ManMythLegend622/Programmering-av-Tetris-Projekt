e52ac9c08aaf40594865feb6f30ac002e83646c3
Robin Johansson <rojo01001@utb.vaxjo.se>
2019-05-05 23:35:09 +0200

    Skapade extra filer för funktionen om hur spelet visar poäng och för hur speletskickar ner blocken. Skapade även en library där alla former och deras egenskaper ska vara.

--- Tetris/Tetris/TetrisGame.cs
+++ Tetris/Tetris/TetrisGame.cs
@@ -7,12 +7,12 @@ namespace Tetris
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class TetrisGame : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public TetrisGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
